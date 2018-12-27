36b7a9b042ded5a708e01fdd2ca4be23464af0b4
Lakshmi Venkat <lakshmi2688@gmail.com>
2018-12-27 10:24:28 -0800

    Added visual studio testng integration files as first commit

---
+++ TestNGIntegrationDemo/ExpediaSearchPage.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNGIntegrationDemo
+{
+    class ExpediaSearchPage
+    {
+        public static IWebElement element = null;
+
+        public static void GetFlyingFrom(IWebDriver driver)
+        {
+            element = driver.FindElement(By.Id("flight-origin-flp"));
+		    element.Clear();
+		    element.SendKeys("SEA");
+	    }
+
+        public static void GetFlyingTo(IWebDriver driver)
+        {
+            element = driver.FindElement(By.Id("flight-destination-flp"));
+            element.Clear();
+            element.SendKeys("LAX");
+        }
+
+
+        public static void GetDeparting(IWebDriver driver)
+        {
+            element = driver.FindElement(By.Id("flight-departing-flp"));
+            element.SendKeys("12/25/2018");
+
+        }
+
+        public static void GetReturning(IWebDriver driver)
+        {
+            element = driver.FindElement(By.Id("flight-returning-flp"));
+            element.Clear();
+            element.SendKeys("03/04/2019");
+        }
+
+        public static void GetTravellers(IWebDriver driver)
+        {
+            element = driver.FindElement(By.XPath("//div[@class='menu-bar gcw-travel-selector-wrapper']//button[contains(@class,'gcw-traveler-amount-select')]"));
+            Actions action = new Actions(driver);
+            action.MoveToElement(element).Click().Perform();
+        }
+
+
+        public static void GetTravellersCount(IWebDriver driver)
+        {
+            GetTravellers(driver);
+            //element = driver.findElement(By.xpath("//div[@class='traveler-selector-sinlge-room-data traveler-selector-room-data']"));
+            element = driver.FindElement(By.XPath("//div[@class='traveler-selector-sinlge-room-data traveler-selector-room-data']//span[contains(text(),'Add Adult')]//parent::button"));
+            Actions action = new Actions(driver);
+            action.MoveToElement(element).Click().Perform();
+            action.MoveToElement(element).Click().Perform();
+            action.MoveToElement(element).Click().Perform();
+            action.MoveToElement(element).Click().Perform();
+
+        }
+
+
+        public static void SubmitButton(IWebDriver driver)
+        {
+            element = driver.FindElement(By.XPath("//div[@id='f-fh-msg-tooltip-flp']//preceding-sibling::label/button"));
+            element.Click();
+        }
+
+}
+}
---
+++ TestNGIntegrationDemo/SeleniumTestSuite.cs
@@ -0,0 +1,49 @@
+﻿using log4net;
+using log4net.Repository.Hierarchy;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNGIntegrationDemo
+{
+    class SeleniumTestSuite
+    {
+
+        IWebDriver driver;
+        string baseUrl = "https://www.expedia.com/Flights";
+        Logger log = (Logger)LogManager.GetLogger("SeleniumTestSuite");
+
+        [SetUp]
+        public void setUp()
+        {
+            driver = new ChromeDriver("C:\\Users\\Lakshmi\\Desktop\\Selenium\\Selenium drivers download\\chromedriver_win32");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = baseUrl;
+            ExpediaSearchPage.GetFlyingFrom(driver);
+            ExpediaSearchPage.GetFlyingTo(driver);
+            ExpediaSearchPage.GetDeparting(driver);
+            ExpediaSearchPage.GetReturning(driver);
+            ExpediaSearchPage.GetTravellersCount(driver);
+            ExpediaSearchPage.SubmitButton(driver);
+        }
+
+
+        [TearDown]
+        public void tearDown()
+        {
+            driver.Quit();
+        }
+
+    }
+}
